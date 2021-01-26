f36cbb02c0b55ff22ded52dc1131bbf4876d86fa
Saraah Bikaï <64023113+saraahbikai-dev@users.noreply.github.com>
2021-01-26 14:26:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 
-const canvas = document.getElementById('canvas1')
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2D');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const edge =80;
+const mouse = {
+  x:null,
+  y:null
+}
+
+window.addEventListener('mousemove',function(event){
+   mouse.x = event.x;
+   mouse.y = event.y;
+   console.log(mouse.x);
+})
+
+class Root {
+  constructor (x,y,color,centerX, centerY){
+}
+
+
+
+
